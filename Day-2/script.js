81aa34924fe2790cf6352a52d75946dc47fc48b2
Sai Siri Kudaravalli <sirikudaravalli24@gmail.com>
2025-06-07 11:56:14 +0530

    Day 1: Introduction to Web Development

---
+++ Day-2/script.js
@@ -0,0 +1,28 @@
+console.log("Hello, World!");
+console.log("How are you all doing?");
+console.log("This is a simple JavaScript file.");
+console.log("Let's log some messages to the console.");
+console.log("JavaScript is fun!");
+console.log("This is the end of the script.");
+console.log("Goodbye!");
+console.log("Thank you for reading!");
+
+let num = "12345";
+var name1 = "MERN";
+console.log(num,name1);
+
+let x = 10;
+let y = 20;
+let sum = x + y;
+console.log("The sum of", x, "and", y, "is", sum);
+let a = 5;
+let b = 15;
+let product = a * b;
+console.log("The product of", a, "and", b, "is", product);
+ischecked = true;
+console.log("Is the checkbox checked?", ischecked);
+
+let var1 = "Hello";
+let var2 = "World";
+let var3 = var1 + " " + var2;
+console.log("Concatenated string:", var3);
